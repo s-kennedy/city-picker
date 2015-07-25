962da9fd608529c630bce81b4adfbe9a5ecce38e
Sharon <sharon.peishan.kennedy@gmail.com>
2015-07-25 15:38:12 +0200

    refactored yelp search methods

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -4,9 +4,11 @@ class SiteController < ApplicationController
   end
 
   def yelp_search
-    search = YelpAPI.new
-    search_result = search.search_by_query(params[:query], params[:city])
-    render :json => search_result
+    city = params[:city]
+    query = params[:query]
+    response = Yelp.client.search(city, { term: query })
+    calc = Calculator.new
+    calc.per_capita(response.total, query, city)
   end
   
 end
